a8ed7be33ec2e90343e68805095b9b1a21508139
Mironov Evgeny <e.d.mironov@yandex.ru>
2026-04-24 21:28:50 +0300

    Импортнул 3 версию спринг бута, что бы полечить проблему невозможности создания бина TestRestTemplate

--- src/test/java/ru/netology/spring_boot_rest/SpringBootRestApplicationTests.java
+++ src/test/java/ru/netology/spring_boot_rest/SpringBootRestApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.resttestclient.TestRestTemplate;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
